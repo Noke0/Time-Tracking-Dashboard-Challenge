bad2b0c3d76272ae52ae60efceb76f489f4a6683
Noke0 <100377376+Noke0@users.noreply.github.com>
2022-04-13 20:36:54 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -23,21 +23,23 @@ async function reloadTime(time){
     let activitiesArr = ["workHours","playHours","studyHours","exerciseHours","socialHours","selfCareHours"];
     let currentHours = [];
     let previousHours = [];
-    let data = await fetch("http://localhost:8000/activities").then(response => {
+    let dataJSON = await fetch("https://noke0.github.io/Time-Tracking-Dashboard-Challenge/data.json").then(response => {
         return response.json();
     });
-    if (time != ""){
-        data.forEach(activity => {
-            currentHours.push(activity.timeframes[time].current);
-            previousHours.push(activity.timeframes[time].previous);
+    Promise.resolve(dataJSON).then(function(data){
+        if (time != ""){
+            data.activities.forEach(activity => {
+                currentHours.push(activity.timeframes[time].current);
+                previousHours.push(activity.timeframes[time].previous);
+            });
+        }
+        let activityNr = 0;
+        activitiesArr.forEach(activity =>{
+            let currentText = document.getElementById(activity);
+            currentText.innerHTML = currentHours[activityNr]+"hrs";
+            let previousText = document.getElementById("prev"+activity);
+            previousText.innerHTML = "Last week - "+previousHours[activityNr]+"hrs";
+            activityNr += 1;
         });
-    }
-    let activityNr = 0;
-    activitiesArr.forEach(activity =>{
-        let currentText = document.getElementById(activity);
-        currentText.innerHTML = currentHours[activityNr]+"hrs";
-        let previousText = document.getElementById("prev"+activity);
-        previousText.innerHTML = "Last week - "+previousHours[activityNr]+"hrs";
-        activityNr += 1;
     });
 }
